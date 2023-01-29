90e21a42cc6d6fa8916136ae72d63fa345845463
Florin Valeanu <valeanu.florin@yahoo.com>
2023-01-29 21:49:27 +0200

    Access the news titles

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
+from send_email import send_email
+
+topic = "tesla"
 
 #url = "https://finance.yahoo.com/"   #stored like astring type
 
 api_key = "3ddff75174784cdab30265dc743ceb28"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2022-12-29&sortBy=publishedAt&apiKey=" \
-      "3ddff75174784cdab30265dc743ceb28"
+url = "https://newsapi.org/v2/everything?"  \
+      f"q={topic}&" \
+      "from=2022-12-29&sortBy=publishedAt&" \
+      "apiKey=3ddff75174784cdab30265dc743ceb28&" \
+      "language=en"
 
 #Make req
 req = requests.get(url)
@@ -14,6 +19,13 @@ req = requests.get(url)
 content = req.json()
 
 #Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+body = ""
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body = "Subject: Today's news" \
+               + "\n" + body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
